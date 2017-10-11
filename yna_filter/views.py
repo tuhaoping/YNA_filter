8180223d558df218064a857282b3ecb8872950f4
Haoping, Tu <31434332+haopingtu@users.noreply.github.com>
2017-10-11 22:26:03 +0800

    csrf cookie
    homepage add ensure_csrf_cookie decorator

--- yna_filter/views.py
+++ yna_filter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def HomePage(request):
 	filter_item = {
 		'Histone Acetylation Patterns ':{
